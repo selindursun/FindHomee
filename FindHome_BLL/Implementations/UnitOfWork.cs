6855c7422351bb0d5ca95f395ff527d38a5f6841
103SABAH_SELİN <103SABAH_SELN@DESK6>
2022-03-24 13:35:37 +0300

    Entityler oluşturuldu. Dal katmanında context classı oluşturuldu ve migration atıldı. BLL katmanında Repository ler tamamlandı.

---
+++ FindHome_BLL/Implementations/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using FindHome_BLL.Contracts;
+using FindHome_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindHome_BLL.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context _context;
+      
+        public UnitOfWork(Context context)
+           
+        {
+            _context = context;
+            
+            //UnitOfWork tüm repositoryleri oluşturacak.
+            HousingRepository = new HousingRepository(_context);
+            HousingTypeRepository = new HousingTypeRepository(_context);
+            OwnerRepository = new OwnerRepository(_context);
+            BuyingTypeRepository = new BuyingTypeRepository(_context);
+           
+        }
+
+        public HousingRepository HousingRepository { get; private set; }
+
+        public HousingTypeRepository HousingTypeRepository { get; private set; }
+
+        public OwnerRepository OwnerRepository { get; private set; }
+
+        public BuyingTypeRepository BuyingTypeRepository { get; private set; }
+
+        IBuyingTypeRepository IUnitOfWork.BuyingTypeRepository => throw new NotImplementedException();
+
+        IHousingTypeRepository IUnitOfWork.HousingTypeRepository => throw new NotImplementedException();
+
+        IHousingRepository IUnitOfWork.HousingRepository => throw new NotImplementedException();
+
+        IOwnerRepository IUnitOfWork.OwnerRepository => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
+
